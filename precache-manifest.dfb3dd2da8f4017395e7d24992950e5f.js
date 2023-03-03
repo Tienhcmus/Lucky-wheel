e68ec16b678e640dc81e129f6f0bb4e6471bd6f0
HRS\tph01 <tien.pham@travel2pay.com>
2023-03-03 16:57:51 +0700

    Updates

---
+++ precache-manifest.dfb3dd2da8f4017395e7d24992950e5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea06ea4d7dbd6cf8d2a5",
+    "url": "/react-gh-pages/static/css/main.31d3d50d.chunk.css"
+  },
+  {
+    "revision": "ea06ea4d7dbd6cf8d2a5",
+    "url": "/react-gh-pages/static/js/main.ea06ea4d.chunk.js"
+  },
+  {
+    "revision": "425b6939921a868c9a62",
+    "url": "/react-gh-pages/static/js/1.425b6939.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "510d638195a08491ccc287146443c32b",
+    "url": "/react-gh-pages/index.html"
+  }
+];
---
+++ static/js/main.ea06ea4d.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{13:function(n,o,c){},15:function(n,o){},2:function(n,o,c){n.exports=c(3)},3:function(n,o,c){"use strict";c.r(o);c(0),c(5),c(13),c(15)}},[[2,2,1]]]);
+//# sourceMappingURL=main.ea06ea4d.chunk.js.map
